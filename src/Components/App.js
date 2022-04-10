8cdfd8f25a8bd19a0bfca83e638150784abf4714
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-04-10 17:45:57 -0400

    functional build

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,7 @@
 import '../Styles/AppLogo.css';
 import '../Styles/App.css';
 import logo from '../Media/logo.svg';
+import Pomodoro from './Pomodoro';
 import Footer from './Footer';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+        <Pomodoro />
       </header>
       <Footer />
     </div>
