84b497d00571853ebf604a2a5bcde44bb8049b72
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-04-12 11:11:01 -0400

    ctl upds readme

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -15,7 +15,7 @@ export default class Footer extends React.Component {
             <footer>
                 <p>Copyright © {this.state.currentYear}
                     <a href="https://www.codebypete.com" target="_blank" rel='noreferrer'>
-                        <img src="https://www.codebypete.com/pics/pharma2code_icon.gif" style={{height:'2rem',padding:'0 0.25rem'}}
+                        <img src="https://codetracklift.github.io/codeTrackLift/logos/pharma2code_icon.gif" style={{height:'2rem',padding:'0 0.25rem'}}
                             alt="codeByPete Logo."/>
                         <span style={{color:"white"}}>code</span><span style={{color:"#00857c"}}>By</span><span
                             style={{color:"lime"}}>Pete</span>
@@ -23,13 +23,13 @@ export default class Footer extends React.Component {
                 </p>
                 <div>
                     <a href="https://github.com/codeTrackLift/markdown-previewer" target="_blank" rel='noreferrer'>
-                        <img className="socialLogo" src="https://www.codebypete.com/pics/contact/GitHub-Mark-Light-64px.png" alt="GitHub Icon"/>
+                        <img className="socialLogo" src="https://codetracklift.github.io/codeTrackLift/logos/GitHub-Mark-Light-64px.png" alt="GitHub Icon"/>
                     </a>
                     <a href="https://twitter.com/codetracklift" target="_blank" rel='noreferrer'>
-                        <img className="socialLogo" src="https://www.codebypete.com/pics/contact/Twitter_social_icons-circle-white.png" alt="Twitter Icon"/>
+                        <img className="socialLogo" src="https://codetracklift.github.io/codeTrackLift/logos/Twitter_social_icons-circle-white.png" alt="Twitter Icon"/>
                     </a>
                     <a href="https://www.linkedin.com/in/codebypete/" target="_blank" rel='noreferrer'>
-                        <img className="socialLogo" src="https://www.codebypete.com/pics/contact/linkedin_white.png" alt="LinkedIn Icon"/>
+                        <img className="socialLogo" src="https://codetracklift.github.io/codeTrackLift/logos/linkedin_white.png" alt="LinkedIn Icon"/>
                     </a>
                 </div>
             </footer>
